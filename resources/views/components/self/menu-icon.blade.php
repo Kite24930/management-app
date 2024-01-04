870b4ff971010424557fdc0b0e88146d03c00199
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2024-01-05 08:52:14 +0900

    index item css draft

--- resources/views/components/self/menu-icon.blade.php
+++ resources/views/components/self/menu-icon.blade.php
@@ -1,3 +1,3 @@
-<a href="{{ route($route) }}" class="md:w-60 w-32 md:h-60 h-32 bg-blue-100 rounded-full text-4xl flex flex-col justify-center items-center hover:bg-blue-300 duration-500">
+<a href="{{ route($route) }}" class="md:w-60 w-32 md:h-60 h-32 rounded-full text-4xl flex flex-col justify-center items-center duration-500 menu-icon">
     {{ $slot }}
 </a>
