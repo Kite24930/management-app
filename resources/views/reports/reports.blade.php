6a3d52ea69e84a416f2fa7ffd6b15ae934ecd226
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2024-06-21 19:58:54 +0900

    reports fix

--- resources/views/reports/reports.blade.php
+++ resources/views/reports/reports.blade.php
@@ -1,7 +1,7 @@
 <x-self.template title="PM Daily Report" css="reports.css">
     <main class="w-full md:pl-80 pt-24 md:pt-4 pb-24 flex flex-col flex-wrap items-center gap-4 relative z-10 full">
         <h1 class="text-3xl text-slate eng-deco">Daily Report</h1>
-        <div class="w-full p-6">
+        <div class="w-full md:p-6 p-0">
             <div class="flex justify-end mb-4">
                 <a href="{{ route('reports.add') }}" class="px-4 py-2 rounded-lg shadow bg-green-100 bg-opacity-70 hover:bg-[#ddbc9577] duration-500 text-xl">New Report</a>
             </div>
@@ -10,7 +10,7 @@
                 <div id="month" class="text-2xl eng-deco">{{ date('Y M') }}</div>
                 <x-secondary-button id="nextMonth">→</x-secondary-button>
             </div>
-            <div id="calendar" class="w-full h-[600px]">
+            <div id="calendar" class="w-full md:h-[600px] h-[1000px]">
 
             </div>
         </div>
